82827b78618537decf0e1121a46c96506ea22b7c
Shahariar Kabir Bhuiyan <shahariar.bhuiyan@mattilsynet.no>
2025-11-25 22:54:50 +0100

    Revert "fix: remove sign up tab"
    This reverts commit 2aea88d76b11cf6644d51646b935be6a0f7603da.

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -24,6 +24,7 @@ export default function AuthForm() {
         <Tabs defaultValue="login" className="w-full">
             <TabsList className="grid w-full grid-cols-2">
                 <TabsTrigger value="login">Login</TabsTrigger>
+                <TabsTrigger value="signup">Sign Up</TabsTrigger>
             </TabsList>
             <TabsContent value="login">
                 <form action={handleLogin} className="space-y-4 pt-4">
